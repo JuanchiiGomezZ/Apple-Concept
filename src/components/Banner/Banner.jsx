289d6fd2cc5f8faf99d04f5509b160addcf146f7
Juan Manuel Gomez Omil <Juanmagomezomil@hotmail.com>
2022-08-25 15:17:06 -0300

    banner was added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="banner">
-      <img src="https://www.apple.com/v/iphone/home/bi/images/overview/hero/iphone_se_hero__gd586pazxqqa_large.jpg" alt="Banner"/>
+      <div className="bannerContainer">
+        <div className="bannerText">
+          <p className="new">New</p>
+          <p className="iphoneSE">iPhone <p className="SE">SE</p></p>
+          <h2 className="loveThe">Love the power.</h2>
+          <h2 className="loveThe">Love the price.</h2>
+          <button>Buy</button>
+        </div>
+        <img
+          src="https://www.apple.com/v/iphone/home/bi/images/overview/hero/iphone_se_hero__gd586pazxqqa_large.jpg"
+          alt="Banner"
+        />
+      </div>
     </div>
   );
 };
