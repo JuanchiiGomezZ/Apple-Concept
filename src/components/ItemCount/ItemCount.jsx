3908c03b5be4d8e96e81f7925f31c0d834c7a34f
Juan Manuel Gomez Omil <Juanmagomezomil@hotmail.com>
2022-09-01 10:21:52 -0300

    Ready to deliver

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,18 +3,25 @@ import React, { useState } from "react";
 const ItemCount = () => {
   const [counter, setCounter] = useState(1);
   let numStock = 7;
-  const [stock, setCounter2] = useState(numStock);
+  const [stock, setStock] = useState(numStock-1);
+  const onAdd = () =>{
+    console.log(counter+1 + ' productos agredado al carro');
+  }
 
   const incremento = () => {
-    if(numStock >= counter){
+    if(numStock > counter){
         setCounter(counter + 1);
-        setCounter2(stock - 1);
+        onAdd()
+        setStock(stock - 1);
+    }
+    else if(stock === 0){
+        alert('No queda mas stock')
     }
   };
   const decremento = () => {
     if(counter > 1){
         setCounter(counter - 1);
-        setCounter2(stock + 1);
+        setStock(stock + 1);
     }
     
   };
